035d75d03862352adb3502f0dd3b8a3b2a35da40
榊原昌彦 <sakakibara@rdlabo.jp>
2019-10-23 18:27:02 +0900

    feat(script): 生成スクリプトを作成

--- scripts/build-dom/index.ts
+++ scripts/build-dom/index.ts
@@ -1,2 +1,65 @@
-import * as core from '@ionic/docs/core'
+import * as docs from '@ionic/docs/core.json';
+import {mkdirSync, writeFileSync, existsSync, readdirSync, readFileSync, lstatSync} from 'fs';
 
+const components = docs.components;
+const componentsDir = 'scripts/ionic/core/src/components/';
+
+// コンポーネントディレクトリを作成
+mkdirSync('./stories');
+
+for (const c of components) {
+  if (!c.tag || !c.usage.javascript || !existsSync(componentsDir + c.tag.replace('ion-', '') + '/test')) {
+    continue;
+  }
+  const tag = c.tag.replace('ion-', '');
+  const dir = readdirSync(componentsDir + tag + '/test');
+
+  let render = `
+        export default {
+          title: '${c.tag}',
+        };
+        `;
+
+  for (const t of dir) {
+    if (!lstatSync(componentsDir + tag + '/test/' + t).isDirectory()) {
+      continue;
+    }
+
+    const tests = readFileSync(componentsDir + tag + '/test/' + t + '/index.html', { encoding: 'UTF8' });
+    let testsHtml = tests.match(/<ion-app[\s\S]*?<\/ion-app>/i);
+    if (!testsHtml) {
+      testsHtml = tests.match(/<body[\s\S]*?<\/body>/i);
+      if (!testsHtml) {
+        console.log('正規表現でとれなかった: ' + componentsDir + tag + '/test/' + t);
+        continue;
+      }
+    }
+
+    const testType = t.replace('-', '_');
+    render = render + `
+        export const ${testType} = () => \`${testsHtml[0].replace(/`/g, '\\`')}\`;
+        `;
+  }
+
+  writeFileSync('stories/' + c.tag + '.stories.js', render, { encoding: 'UTF8' });
+
+
+  //
+  //
+  // let usage = JSON.stringify(c.usage.javascript)
+  //   .replace('```html', '')
+  //   .replace('```', '');
+  //
+  // if (!usage.match(/ion-app/)) {
+  //   usage = '"<ion-app>' + usage.slice( 1 ).slice( 0, -1 ) + '</ion-app>"'
+  // }
+  //
+  // const render = `
+  //   export default {
+  //     title: '${c.tag}',
+  //   };
+  //   export const components = () => ${usage};
+  //   `;
+  //
+  // writeFileSync('stories/' + c.tag + '.stories.js', render, { encoding: 'UTF8' });
+}
